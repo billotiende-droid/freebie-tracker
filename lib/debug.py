c3dea42fcead627e1b2054638ff36d983b495833
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-01 22:49:02 +0300

    freebie-dev-freebie-company-print-statements

--- lib/debug.py
+++ lib/debug.py
@@ -11,9 +11,35 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
+
+print("\n===ALL DATA FROM DATABASE===")   
+ 
 print(session.query(Company).all())
 print(session.query(Dev).all())
 print(session.query(Freebie).all())
+
+print("n\=== TESTING RELATIONSHIPS & METHODS ===")
+
+# freebie.dev + freebie.company
+
+freebie = session.query(Freebie).first()
+
+print("Freebie:", freebie)
+print("Developer:", freebie.dev)
+print("Company:", freebie.company)
+print("print_details():", freebie.print_details())
+
+# company.devs + companies.freebies
+print("\n COMPANY -> DEVS/FEEBIES")
+
+company = session.query(Company).first()
+
+print("Company:", company)
+print("Developers:", company.devs)
+print("Freebies:", company.freebies)
+
+
   
-import ipdb; ipdb.set_trace()
+# import ipdb; ipdb.set_trace()
   
